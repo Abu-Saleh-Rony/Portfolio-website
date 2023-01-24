4b509b638b3a5693632bff2212028cb8d0f1ef40
abusalehrony <abusalehrony12@gmail.com>
2023-01-24 19:35:22 +0600

    added Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
-      <h2 className="text-black text-4xl font-signature">Abu Saleh Rony</h2>
+      <NavBar></NavBar>
 
     </div>
   );
